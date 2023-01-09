bde5d90662bd1be11389bbcc13a53f4d2cd06fcb
odaialazzeh <odai.alazzeh@gmail.com>
2023-01-09 19:52:21 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Missions from './Pages/MISSIONS';
+import Rockets from './Pages/Rockets';
+import Profile from './Pages/PROFILE';
+import Dragons from './Pages/DRAGONS';
+import Navbar from './component/Navbar';
 
-import './App.css';
-
-function App() {
- 
-};
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route index element={<Rockets />} />
+        <Route path="/Missions" element={<Missions />} />
+        <Route path="/Dragons" element={<Dragons />} />
+        <Route path="/Profile" element={<Profile />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
