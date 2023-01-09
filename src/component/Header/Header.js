5d4bc37abb42aae23b59a9cbe3febbdfccc50782
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-01-09 19:19:02 +0430

    import NavLink from react-router, logo, and Header.css file

---
+++ src/component/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from './image/logo.png';
+import '../Header.css';
+
+const Header = () => {
+
+};
+
+export default Header;
