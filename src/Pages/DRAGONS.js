11764e076879d042a9334616c3cd6849f5df5314
odaialazzeh <odai.alazzeh@gmail.com>
2023-01-10 19:24:03 +0200

    Update and display dragons

--- src/Pages/DRAGONS.js
+++ src/Pages/DRAGONS.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getDragons } from '../redux/dragonReducer';
+import Dragon from '../component/dragon';
 
-const Dragons = () => (
-  <div className="Dragons-container" />
-);
+const Dragons = () => {
+  const dispatch = useDispatch();
+  const dragonDetails = useSelector((state) => state.dragons);
+  useEffect(() => {
+    if (!dragonDetails.length) dispatch(getDragons());
+  }, [dispatch, dragonDetails]);
+
+  return (
+    <ul>
+      {dragonDetails.map((dragon) => (
+        <Dragon
+          key={dragon.id}
+          dragonsName={dragon.name}
+          imgURL={dragon.flickr_images}
+          description={dragon.description}
+          type={dragon.type}
+        />
+      ))}
+    </ul>
+  );
+};
 
 export default Dragons;
