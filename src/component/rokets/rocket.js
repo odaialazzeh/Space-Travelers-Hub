fd3a4cefb3369fe37f0bb2571c42c86965841dd0
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-01-11 12:47:09 +0430

    add useeffect

--- src/component/rokets/rocket.js
+++ src/component/rokets/rocket.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import RocketCard from './rocketCard';
+import { getRockets } from '../../redux/rockets/rockets';
 import './rocket.css';
-import rocketCard from './rocketCard';
 
 const Rocket = () => {
+  const rockets = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    if (rockets.rocketsData.length === 0) {
+      dispatch(getRockets());
+    }
+  }, []);
+  const newRockets = rockets.rocketsData;
+
+  const cards = newRockets.length > 0 && newRockets.map((rocket) => (
+    <RocketCard
+      key={rocket.id}
+      id={rocket.id}
+      name={rocket.name}
+      image={rocket.image}
+      desc={rocket.desc}
+      reserved={rocket.reserved}
+    />
+  ));
+
+  return (
+    <div className="rocket-container">
+      {cards}
+    </div>
+  );
 };
 
 export default Rocket;
