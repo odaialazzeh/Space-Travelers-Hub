72f0e8fad75a2ecef059ca18bd73e22f7b0dc2ab
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-01-11 12:39:41 +0430

    add getRoockets function

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,25 +1,46 @@
-import FetchRocketsData from "../API/rocketAPI";
+import FetchRocketsData from '../API/rocketAPI';
+
 const GET_ROCKETS_REQUEST = 'space-travellers/rockets/GET_REQUEST';
 const GET_ROCKETS_SUCCESS = 'space-travellers/rockets/GET_SUCESS';
 const GET_ROCKETS_FAIL = 'space-travellers/rockets/GET_FAIL';
 const RESERVE_ROCKET = 'space-travellers/rockets/RESERVE_ROCKET';
 
 const initialState = {
-    rocketsData: [],
-    error: '',
-    load: false,
+  rocketsData: [],
+  error: '',
+  load: false,
+};
+
+const getRocketRequest = () => ({
+  type: GET_ROCKETS_REQUEST,
+});
+
+const getRocketSuccess = (rocketsData) => ({
+  type: GET_ROCKETS_SUCCESS,
+  payload: rocketsData,
+});
+
+const getRocketFail = (error) => ({
+  type: GET_ROCKETS_FAIL,
+  payload: error,
+});
+
+export function getRockets() {
+  return (dispatch) => {
+    dispatch(getRocketRequest());
+    FetchRocketsData()
+      .then((data) => {
+        const rocketinfo = data.map((uniData) => ({
+          id: uniData.rocket_id,
+          name: uniData.rocket_name,
+          desc: uniData.description,
+          image: uniData.flickr_images[0],
+          reserved: false,
+        }));
+        dispatch(getRocketSuccess(rocketinfo));
+      })
+      .catch((error) => {
+        dispatch(getRocketFail(error.message));
+      });
   };
-  
-  const getRocketRequest = () => ({
-    type: GET_ROCKETS_REQUEST,
-  });
-  
-  const getRocketSuccess = (rocketsData) => ({
-    type: GET_ROCKETS_SUCCESS,
-    payload: rocketsData,
-  });
-  
-  const getRocketFail = (error) => ({
-    type: GET_ROCKETS_FAIL,
-    payload: error,
-  });
+}
