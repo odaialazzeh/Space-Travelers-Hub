01d10ac13efd8a43e916b43fbdeeb130d9b1150e
odaialazzeh <odai.alazzeh@gmail.com>
2023-01-11 20:18:28 +0200

    Add profile component

---
+++ src/component/profile.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { reserve } from '../redux/dragonReducer';
+
+const MyProfile = ({
+  id, dragonsName, reserved, infoURL,
+}) => {
+  const dispatch = useDispatch();
+  return (
+    <ul>
+      <li>
+        {dragonsName}
+
+        <button
+          type="button"
+          className={`${reserved ? 'cancel' : 'reserve'}`}
+          onClick={() => dispatch(reserve({ id }))}
+        >
+          {reserved ? 'Cancel Reservation' : 'Reserve Dragon'}
+        </button>
+        <a href={infoURL}>Read more</a>
+      </li>
+    </ul>
+  );
+};
+
+MyProfile.propTypes = {
+  dragonsName: PropTypes.string.isRequired,
+  reserved: PropTypes.bool.isRequired,
+  id: PropTypes.string.isRequired,
+  infoURL: PropTypes.string.isRequired,
+};
+
+export default MyProfile;
