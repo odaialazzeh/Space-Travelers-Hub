4da0c3cc70d2001ac181e70895f098d6b9baa906
Victoria Tumwebaze <victoria.tumwebaze@outlook.com>
2023-01-12 14:23:08 +0300

    Add Missions.js and missionsReducer.js

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import missions from './missionsReducer';
+import dragonsReducer from './dragonReducer';
+import rocketReducer from './rockets/rockets';
 
 const store = configureStore({
   reducer: {
+    dragons: dragonsReducer,
+    rockets: rocketReducer,
     missions,
   },
 
