ac134a98fdceea9bd5be99aea4de0a097564322b
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-01-12 12:14:42 +0430

    add provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-/* eslint-disable import/no-named-as-default-member */
-import React from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import App from './App';
-import './styles.css';
 
-const root = createRoot(document.getElementById('root'));
+// 👇️ IMPORTANT: use correct ID of your root element
+// this is the ID of the div in your index.html file
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+// 👇️ if you use TypeScript, add non-null (!) assertion operator
+// const root = createRoot(rootElement!);
+
 root.render(
-  <App />,
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>,
+
 );
