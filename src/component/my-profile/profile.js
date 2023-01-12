9f7a0ec3f8f26307145d015c56c169cf734d8c57
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-01-12 17:15:26 +0430

    add handlerReservation function for button '

--- src/component/my-profile/profile.js
+++ src/component/my-profile/profile.js
@@ -5,3 +5,14 @@ import './profile.css';
 const MyProfile = () => {
     const myRockets = useSelector((state) => state.rockets);
     const myReserveRockets = myRockets.rocketsData.filter((rocket) => rocket.reserved);
+
+    const handleReservations = () => {
+        if (myReserveRockets.length) {
+          return myReserveRockets.map((rocket) => (
+            <p key={rocket.id} className="my-rocket">
+              {rocket.name}
+            </p>
+          ));
+        }
+        return <h2>No Reserved Rockets</h2>;
+      };
